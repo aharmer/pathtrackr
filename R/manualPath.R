81dfc2c8e5a45b485180a8ab53ee5b2c2db57662
Aaron Harmer <aharmer@it076210.massey.ac.nz>
2019-02-19 16:53:52 +1300

    Updated manualPath function with ability to handle occlusions, clarified instructions, added check for file existence.

--- R/manualPath.R
+++ R/manualPath.R
@@ -11,17 +11,37 @@
 #' @export
 manualPath = function(dirpath, xarena, yarena, fps = 30) {
 
-  file.list = list.files(dirpath, full.names = TRUE)
+  require(raster, quietly = TRUE, warn.conflicts = FALSE)
 
-  bg.dim = dim(greyJPEG(file.list[1]))
+  if (length(dir(dirpath, "*.jpg")) > 0) {
+    file.list = list.files(dirpath, full.names = TRUE)
+  } else {
+    stop("No files were found... check that the path to your directory is correct and that it contains only jpg files.")
+  }
+
+  # Crop array to area of interest if needed
+  message("Click once on the top left corner of your arena, followed by clicking once on the bottom right corner of your arena, to define the opposing corners of the entire arena...\n")
+  flush.console()
+  plot(raster(file.list[1], band = 2), col = gray.colors(256), asp = 1, legend = FALSE)
+  bg.crop = base::as.vector(extent(select(raster(file.list[1], band = 2))))
+
   xpos = c()
   ypos = c()
 
+  message("Click once on the animal in each frame. Choose an easily identifiable part of the animal for consistency, e.g. the front of the head. Frames will advance automatically...\n")
+  flush.console()
+
   for (i in 1:length(file.list)) {
-    par(mar = c(1,0,0,0))
-    plot(raster(file.list[i], band = 2), col = gray.colors(256), asp = 1, legend = FALSE)
-    mtext("x", side = 1, line = 0, adj = 0.0, cex = 2, col = "red", outer = TRUE)
-    mtext("Click the 'x' if the animal is not visible", side = 1, line = 1, adj = 0.0, cex = 1, col = "red", outer = TRUE)
+    par(mar = c(7,5,0,0))
+
+    bg.ref = greyJPEG(file.list[i])
+    bg.ref = bg.ref[(dim(bg.ref)[1] - bg.crop[3]):(dim(bg.ref)[1] - bg.crop[4]), bg.crop[1]:bg.crop[2]]
+    bg.dim = dim(bg.ref)
+    plot(raster(reflect(bg.ref), xmn = 0, xmx = bg.dim[2], ymn = 0, ymx = bg.dim[1]), col = gray.colors(256), asp = 1, legend = FALSE)
+
+    mtext("x", side = 1, line = 3, adj = 0.0, cex = 2, col = "red", at = -25)
+    mtext("Click the 'x' if the animal is not visible", side = 1, line = 4, adj = 0.0, cex = 1, col = "red", at = -25)
+    mtext(paste(i, "/", length(file.list), sep = ""), side = 1, line = 2, adj = 0.0, cex = 1, col = "darkgreen", at = -25)
     temp = locator(1)
     xpos[i] = round(temp[[1]])
     ypos[i] = round(temp[[2]])
